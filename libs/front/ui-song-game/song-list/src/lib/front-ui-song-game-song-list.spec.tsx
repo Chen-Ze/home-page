0851c89a7ec56b49ffb723395fe73d648606e80d
Chen Ze <chenze_ustc@mail.ustc.edu.cn>
2022-02-11 02:30:30 +0800

    test(front-ui-song-game-song-list.spec.tsx): mock scrollIntoView

--- libs/front/ui-song-game/song-list/src/lib/front-ui-song-game-song-list.spec.tsx
+++ libs/front/ui-song-game/song-list/src/lib/front-ui-song-game-song-list.spec.tsx
@@ -5,6 +5,12 @@ import { generateSongList } from '@home-page/front/ui-song-game/fake-data';
 
 describe('FrontUiSongGameSongList', () => {
   it('should render successfully', () => {
+    // we have to mock scrollIntoView here
+    // see {@link https://stackoverflow.com/questions/51527362/testing-scrollintoview-jest}
+    // or {@link https://github.com/jsdom/jsdom/issues/1695}
+    const scrollIntoViewMock = jest.fn();
+    HTMLElement.prototype.scrollIntoView = scrollIntoViewMock;
+
     const { baseElement } = render(
       <FrontUiSongGameSongList
         onSelect={() => {
